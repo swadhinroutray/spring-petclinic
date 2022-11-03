b40d7fbddd18248a6f6e7b69d299243186551764
swadhinroutray <swadhin.routray@gmail.com>
2022-11-03 15:07:53 -0400

    sonarqube

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
-./mvnw spring-boot:build-image
-
-docker tag spring-petclinic:2.7.3 swadhinroutray/spring-petclinic
-docker commit spring-petclinic:2.7.3 swadhinroutray/spring-petclinic
-docker push swadhinroutray/spring-petclinic
-
-
-
+docker run --name sonarqubeContainer --restart always -p 9000:9000 -d sonarqube
+docker-compose -f deploy.docker-compose.yml -d up
