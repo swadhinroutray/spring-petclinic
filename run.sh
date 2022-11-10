2df33c712e95edff9711a322844b73a4a7b96b24
swadhinroutray <swadhin.routray@gmail.com>
2022-11-10 14:44:58 -0500

    Remove Vagrantfile

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-
-git clone 
+echo "System Limits Established"
 
 sysctl -w vm.max_map_count=524288
 sysctl -w fs.file-max=131072
 
-docker-compose -f deploy.docker-compose.yml up -d
+echo "Starting Containers"
+
+docker-compose -f deploy.docker-compose.yml up
