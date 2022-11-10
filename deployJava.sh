d759595990fa756487b04d96445ce7828ca0ff35
swadhinroutray <swadhin.routray@gmail.com>
2022-11-10 15:29:04 -0500

    Executable Deploy

--- deployJava.sh
+++ deployJava.sh
@@ -1,3 +1,10 @@
+echo "Install Java and Deploy JAR"
+sudo apt update
+sudo apt install default-jre
+sudo apt update
+sudo apt install default-jdk
+javac -version
+
 echo "Install Latest Petclinic JAR"
 wget http://167.172.157.150:8080/job/spring/lastSuccessfulBuild/artifact/target/spring-petclinic-2.7.3.jar
 
