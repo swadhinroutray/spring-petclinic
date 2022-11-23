39394a1ad289c4bcf67bb4eed6fc02c965e04e2b
swadhinroutray <swadhin.routray@gmail.com>
2022-11-23 17:53:09 -0500

    Ansible

--- deployJava.sh
+++ deployJava.sh
@@ -5,9 +5,6 @@ sudo apt update
 sudo apt install default-jdk
 javac -version
 
-echo "Install Latest Petclinic JAR"
-wget http://167.172.157.150:8080/job/spring/lastSuccessfulBuild/artifact/target/spring-petclinic-2.7.3.jar
-
 echo "Deploy Petclinic JAR"
 ufw allow 8083 
 java -jar target/*.jar --server.port=8083
--- run.sh
+++ run.sh
@@ -5,8 +5,7 @@ sysctl -w fs.file-max=131072
 
 echo "Starting Containers"
 
-docker-compose -f deploy.docker-compose.yml up
+docker-compose -f deploy.docker-compose.yml up -d
 
 echo "Expose SonarQube Server Port for HTTP requests from Jenkins"
-
 ufw allow 9000
